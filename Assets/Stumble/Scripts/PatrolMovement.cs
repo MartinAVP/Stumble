eb0d6a39af3c9cab32997624596337705b618284
Shane M <Dragondragon.wu21@gmail.com>
2024-09-17 16:52:17 -0700

    cube stuff

--- Assets/Stumble/Scripts/PatrolMovement.cs
+++ Assets/Stumble/Scripts/PatrolMovement.cs
@@ -8,13 +8,18 @@ public class PatrolMovement : MonoBehaviour
     private int CurrentNode = 0;
     public List<Transform> PatrolNodes = new List<Transform>();
     public float speed = 2f;
+    public float rotationSpeed = 2f;
+
+    private GameObject player;
 
     public bool faceTowardNextNode = false;
 
     private float delayTime = 1f;
     private float delayCounter = 0f;
     private bool delayed = false;
-    
+
+    private Quaternion nodeRotation;
+
     void FixedUpdate()
     {
         if (delayed == true)
@@ -44,8 +49,52 @@ public class PatrolMovement : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, nextNode.position, speed * Time.deltaTime);
             if (faceTowardNextNode == true)
             {
-                transform.LookAt(nextNode.position);
+                nodeRotation = Quaternion.LookRotation(nextNode.position - transform.position);
+                float rotationStrength = Mathf.Min(rotationSpeed * Time.deltaTime, 1);
+                transform.rotation = Quaternion.Lerp(transform.rotation, nodeRotation, rotationStrength);
+
+                Debug.Log(player);
+
+                if (player != null)
+                {
+                    //player.transform.rotation = Quaternion.identity;
+
+                    //Debug.Log(nodeRotation);
+
+                    // old "rotation" - cube will snap into direction
+                    //transform.LookAt(nextNode.position);
+                }
             }
         }
+
+        //Debug.Log(player.transform.position);
     }
+
+    //Player parrenting on collision with plat. 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("connection");
+            player = collision.gameObject;
+            Debug.Log("Player Object should not be null: " + player.transform.rotation);
+
+            collision.collider.transform.SetParent(transform);
+
+        }
+    } 
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("seperation");
+
+            Debug.Log("Player Object: " + player);
+            player = null;
+            Debug.Log("Player Object null: " + player);
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
 }
