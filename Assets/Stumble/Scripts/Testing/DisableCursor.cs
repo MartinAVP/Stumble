e87d37be47b57b913e16d96e0a416dd2fe9f13e4
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2024-09-17 23:01:06 -0700

    Tuned player controller parameters

--- Assets/Stumble/Scripts/Testing/DisableCursor.cs
+++ Assets/Stumble/Scripts/Testing/DisableCursor.cs
@@ -7,6 +7,7 @@ public class DisableCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 }
