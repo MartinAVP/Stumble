153834dd989aeea58b77f96c9344bebaafce95f8
Shane M <Dragondragon.wu21@gmail.com>
2024-09-25 20:08:39 -0700

    car spawner

--- Assets/Stumble/Scripts/CarSpawner.cs
+++ Assets/Stumble/Scripts/CarSpawner.cs
@@ -5,55 +5,134 @@ using UnityEngine;
 public class CarSpawner : MonoBehaviour
 {
     public GameObject CarPrefab;
+    public List<GameObject> CarLanes = new List<GameObject>();
+
+    public int ActiveLanes;
 
     public bool Triggered = false;
 
-    //delay before next flip
-    private float delayTime = 1f;
-    private float delayCounter = 0f;
+    public float MinTimeBetweenCars = 1f;
+    public float MaxTimeBetweenCars = 2f;
+    
+    public bool WavesMode = false;
+    public float FlatWaveDelay = 4f;
+
+    private bool spawning = false;
     private bool delayed = false;
 
-    public GameObject LaneOne;
-    public GameObject LaneTwo;
-    public GameObject LaneThree;
-    public GameObject LaneFour;
-
-    public List<GameObject> CarLanes = new List<GameObject>();
-
-    public int ActiveLanes = 2;
+    public bool RandomTimingWaves = false;
+    public float MinTimeBetweenWaves = 5f;
+    public float MaxTimeBetweenWaves = 10f;
 
     private int CarSpawnerCounter = 0;
 
     private int CarsSpawned = 0;
+    private int S = 0;
 
-
-    void Start()
-    {
-        Debug.Log(CarLanes[1].transform.position);
-    }
+    private int Prev_S;
 
 
     void FixedUpdate()
     {
         if (Triggered == true)
         {
-            while (CarSpawnerCounter < ActiveLanes)
+            // accounts for the lack of first case (int for Prev_S) for the beginning of the spawn cycles
+            if (CarsSpawned == 0)
             {
-                CarsSpawned++;
-                int S = Random.Range(0, 3);
-                Debug.Log("Going to instantiate " + CarPrefab + " at " + CarLanes[S].transform.position + " with " + Quaternion.identity + " rotation");
+                S = Random.Range(0, 3);
+                //Debug.Log("Random Lane: " + S);
+                Prev_S = S;
+
                 Instantiate(CarPrefab, CarLanes[S].transform.position, Quaternion.identity);
-                Debug.Log("Instantiated " + CarPrefab + " at " + CarLanes[S].transform.position + " with " + Quaternion.identity + " rotation");
+                CarsSpawned++;
 
-
-                Debug.Log("Car Count: " + CarsSpawned);
                 CarSpawnerCounter++;
+                //Debug.Log("Car Count (Starter): " + CarsSpawned);
 
+                StartCoroutine(Delay(true, true));
+
+                //Debug.Log("start finished");
+            }
+            if (spawning)
+            {
+                //Debug.Log("b");
+                spawning = false;
+                StartCoroutine(Delay(true, false));
             }
         }
     }
 
 
+    //delay between "waves"
+    private IEnumerator Delay(bool car, bool wave) 
+    {
+        if (car)
+        {
+            while (CarSpawnerCounter < ActiveLanes)
+            {
+                float CT = Random.Range(MinTimeBetweenCars, MaxTimeBetweenCars);
+                //Debug.Log(CT);
+                yield return new WaitForSecondsRealtime(CT);
+                //Debug.Log("car timer done");
+
+                int Prev_Prev_S = Prev_S;
+                Prev_S = S;
+
+                if (ActiveLanes >= 3)
+                {
+                    while (S == Prev_S || S == Prev_Prev_S)
+                    {
+                        S = Random.Range(0, 3);
+                    }
+                }
+                else
+                {
+                    while (S == Prev_S)
+                    {
+                        S = Random.Range(0, 3);
+                    }
+                }
+                
+                Instantiate(CarPrefab, CarLanes[S].transform.position, Quaternion.identity);
+                //Debug.Log("Instantiated " + CarPrefab + " at " + CarLanes[S].transform.position + " with " + Quaternion.identity + " rotation");
+                CarsSpawned++;
+                CarSpawnerCounter++;
+
+                Debug.Log("Car Count: " + CarsSpawned);
+                if (CarSpawnerCounter == ActiveLanes)
+                {
+                    //Debug.Log("wave complete");
+                }
+            }
+        }
+
+        if (WavesMode) //if (wave && WavesMode)
+        {
+            //randomizes the timer after the first spawn wave
+            if (RandomTimingWaves == true)
+            {
+                float WT = Random.Range(MinTimeBetweenWaves, MaxTimeBetweenWaves);
+                //Debug.Log(WT);
+                yield return new WaitForSecondsRealtime(WT);
+                spawning = true;
+                CarSpawnerCounter = 0;
+
+            }
+            else
+            {
+                //Debug.Log("c");
+                yield return new WaitForSecondsRealtime(FlatWaveDelay);
+                spawning = true;
+                CarSpawnerCounter = 0;
+            }
+        }
+
+        if (WavesMode != true) 
+        {
+            spawning = true;
+            CarSpawnerCounter = 0;
+        }
+    }
 
     //spawner
 
