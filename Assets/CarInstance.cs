cb27a9fc0220071b4b9d1204b68fda167803b566
Shane M <Dragondragon.wu21@gmail.com>
2024-10-01 16:03:45 -0700

    10/1 pushe

--- Assets/CarInstance.cs
+++ Assets/CarInstance.cs
@@ -16,16 +16,34 @@ public class CarInstance : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(wheel1.transform.position);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Quaternion localRotation = Quaternion.Euler(-rpm * Time.deltaTime, 0f, 0f);
-        wheel1.transform.rotation = wheel1.transform.rotation * localRotation;
+        wheel1.transform.localRotation = Quaternion.Euler(-rpm * Time.deltaTime, 0f, 0f);
+        
+        //wheel3.transform.rotation = wheel1.transform.rotation * localRotation;
+        //wheel4.transform.rotation = wheel1.transform.rotation * localRotation;
         Debug.Log("spin");
 
+        CarPositioning();
+    }
 
+    private void CarPositioning()
+    {
+        Vector3 FrontLeft = wheel1.transform.position; // - Vector3.forward * 1.2f + Vector3.right * 0.9f;
+        Vector3 FrontRight = wheel2.transform.position; // - Vector3.forward * 1;
+        Vector3 BackLeft = transform.position + Vector3.right * 1;
+        Vector3 BackRight = transform.position - Vector3.right * 1;
+
+        Vector3 delta = Vector3.down * ((0.5f * 1) + .2f);
+
+        Debug.DrawLine(FrontLeft, FrontLeft + delta, Color.red);
+        Debug.DrawLine(FrontRight, FrontRight + delta, Color.blue);
+        Debug.DrawLine(BackLeft, BackLeft + delta, Color.green);
+        Debug.DrawLine(BackRight, BackRight + delta, Color.magenta);
     }
 }
