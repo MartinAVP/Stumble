336729cb526f42dd684f55760acc4e0e953b62ca
Shane M <Dragondragon.wu21@gmail.com>
2024-10-10 15:05:10 -0700

    Car Spawner

--- Assets/Stumble/Scripts/HungeryHippo.cs
+++ Assets/Stumble/Scripts/HungeryHippo.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class HungeryHippo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float hippoMouthSpeed = 10f;
+    public float hippoMoveSpeed = 2;
+
+    public GameObject playerKillzone;
+
+    public bool triggered = false;
+
+    public bool reset = false;
+
+
+
+
+
+
+    void FixedUpdate()
     {
+        if (triggered)
+        {
+            if (transform.localEulerAngles.x > 320 || transform.localEulerAngles.x < 310)
+            {
+
+
+                Debug.Log("localEuler: " + transform.localEulerAngles.x);
+                Quaternion mouthRotation = Quaternion.Euler(-hippoMouthSpeed * Time.deltaTime, 0f, 0f);
+                Debug.Log("mouthRot: " + mouthRotation);
+
+                this.transform.rotation = this.transform.rotation * mouthRotation;
+                Debug.Log("localEuler: " + this.transform.rotation);
+            }
+
+        }
+
+        if (reset)
+        {
+            reset = false;
+            this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
         
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
+
+    // trigger
+
+    //open mouth
+
+    //shoot forward (optional)
+
+    //close
+
+    //kill box activates
+
+    //retracts (optional)
+
+    //kill box deactivates
+
+    //repeat
+
 }
