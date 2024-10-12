76aa65675a2ebdbf9b06e487344bc415f6bcebc7
Martin Vilches Perez <129893589+MartinAVP@users.noreply.github.com>
2024-10-12 01:48:54 -0700

    Audio Start

---
+++ Assets/Stumble/Scripts/Misc/Audio Managers/MenuMusicController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicController : MonoBehaviour
+{
+
+    public static MenuMusicController Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+}
