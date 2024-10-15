4e317a8ef0b80480186c4d877b3e48bbc72b32bb
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2024-10-15 00:39:53 -0700

    Removed 30fps cap

--- Assets/Stumble/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Stumble/Scripts/Player/ThirdPersonMovement.cs
@@ -97,8 +97,6 @@ public class ThirdPersonMovement : MonoBehaviour, IBumper
 
     private void OnEnable()
     {
-        Application.targetFrameRate = 30;
-
         if(playerMovementSettings == null)
         {
             Debug.LogError("All the variables were changed to default due to the third person controller not having a player card attached");
@@ -285,6 +283,8 @@ public class ThirdPersonMovement : MonoBehaviour, IBumper
     /// </summary>
     private void Movement()
     {
+        _bumpHorizontalVelocity.y = 0;
+
         ApplyGravity();
         ApplyVerticalMovement();
         isGrounded();
@@ -474,20 +474,11 @@ public class ThirdPersonMovement : MonoBehaviour, IBumper
     {
         if (lockVeritcalMovement) { return; }
 
-        // Prone Logic
-        if (isProne)
-        {
-            if (!_grounded)
-            {
-                verticalVelocity += _gravity * gravityMultiplier * Time.deltaTime;
-            }
 
-            return;
-        }
 
-        if (_grounded && verticalVelocity < -5f)
+        if (_grounded && verticalVelocity < 0)
         {
-            verticalVelocity = -5.0f;  // Prevents the character from sinking into the ground
+            verticalVelocity = 0;  // Prevents the character from sinking into the ground
         }
         else
         {
@@ -544,7 +535,7 @@ public class ThirdPersonMovement : MonoBehaviour, IBumper
             // 
         }
 
-        Vector3 delta = Vector3.down * ((0.5f * playerHeight) + .1f);
+        Vector3 delta = Vector3.down * ((0.5f * playerHeight) + .2f);
 
         Debug.DrawLine(start1, start1 + delta, Color.red);
         Debug.DrawLine(start2, start2 + delta, Color.blue);
@@ -655,6 +646,8 @@ public class ThirdPersonMovement : MonoBehaviour, IBumper
 
         if (_grounded == false) { _grounded = controller.isGrounded; };
 
+        print("Is player grounded? " + _grounded);
+
         //return _grounded;
     }
 
@@ -697,7 +690,7 @@ public class ThirdPersonMovement : MonoBehaviour, IBumper
         controller.enabled = true;
 
         platformVelocity = (transform.position - startPos) / currentPlatform.DeltaTime;
-        platformVelocity.y = 0;
+        //platformVelocity.y = 0;
     }
 
 
