988b061b342a4dc747ec872601677eb14cd792ca
MartinAVP <mv407692@gmail.com>
2024-10-24 15:51:17 -0700

    Level Setup

--- Assets/Stumble/Scripts/NewDev/ModularGamemodes.cs
+++ Assets/Stumble/Scripts/NewDev/ModularGamemodes.cs
@@ -8,14 +8,14 @@ public class ModularGamemodes : MonoBehaviour
 {
     public List<Gamemode> modules;                                          // List of Module Gamemodes 
     [Header("Selection")]
-    public List<Gamemode> chosenGamemodes = new List<Gamemode>();           // List of Chosen Module Gamemodes
+    public List<Gamemode> activeGamemodes = new List<Gamemode>();           // List of Chosen Module Gamemodes
 
     [SerializeField]private int targetSelection;
     public bool allowDuplicates = false;
 
     private void OnDisable()
     {
-        chosenGamemodes.Clear();
+        activeGamemodes.Clear();
     }
 
     private void Start()
@@ -23,32 +23,32 @@ public class ModularGamemodes : MonoBehaviour
 
     }
 
-    private void OnGUI()
+/*    private void OnGUI()
     {
         // Set the position and size of the button
         if (GUI.Button(new Rect(10, 10, 150, 30), "Select Gamemodes"))
         {
-            chosenGamemodes.Clear();
+            activeGamemodes.Clear();
             SelectGamemodes(targetSelection);
             //Debug.Log("Select Random Gamemodes");
         }
-    }
+    }*/
 
-    private void SelectGamemodes(int quantity)
+    public void SelectGamemodes(int quantity)
     {
         RecursiveSelect(quantity);
     }
 
     private void RecursiveSelect(int quantity)
     {
-        if (chosenGamemodes.Count >= quantity) { return; } // Finished Doing Recursion
-        if(chosenGamemodes.Count == modules.Count) { Debug.LogWarning("Could not continue Recursion, missing asset quantity. Tried getting " + quantity + ", but there are only " + modules.Count + "."); return; } // Finish Recursion - Modules List is not filled enough.
-        if(chosenGamemodes.Count >= GetEnabledMapsCount()) { return; }  // Check if there is no more enabled maps available
+        if (activeGamemodes.Count >= quantity) { return; } // Finished Doing Recursion
+        if(activeGamemodes.Count == modules.Count) { Debug.LogWarning("Could not continue Recursion, missing asset quantity. Tried getting " + quantity + ", but there are only " + modules.Count + "."); return; } // Finish Recursion - Modules List is not filled enough.
+        if(activeGamemodes.Count >= GetEnabledMapsCount()) { return; }  // Check if there is no more enabled maps available
 
         int id = Random.Range(0, modules.Count); // Get a random index from the modules list
 
         // Check for duplicates
-        if (chosenGamemodes.Contains(modules[id]) && !allowDuplicates)
+        if (activeGamemodes.Contains(modules[id]) && !allowDuplicates)
         {
             RecursiveSelect(quantity);
             return;
@@ -63,7 +63,7 @@ public class ModularGamemodes : MonoBehaviour
 
         else
         {
-            chosenGamemodes.Add(modules[id]);
+            activeGamemodes.Add(modules[id]);
             //Debug.Log($"Added: {modules[id].name}, Current Count: {chosenGamemodes.Count}");
             RecursiveSelect(quantity);
         }
