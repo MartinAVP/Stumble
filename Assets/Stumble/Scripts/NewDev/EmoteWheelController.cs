e565631ad9be2cb3ec467f3bfe29a08150aabab1
MartinAVP <mv407692@gmail.com>
2024-10-31 11:48:53 -0700

    More Wheel Progress

--- Assets/Stumble/Scripts/NewDev/EmoteWheelController.cs
+++ Assets/Stumble/Scripts/NewDev/EmoteWheelController.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class EmoteWheelController : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class EmoteWheelController : MonoBehaviour
     private string controlScheme;
     [SerializeField] private GameObject pointerCenter;
     [SerializeField] private Transform viewer;
+    [SerializeField] private Transform[] wheelPieces;
+
+    private bool isActive = false;
 
     private void Start()
     {
@@ -41,9 +45,99 @@ public class EmoteWheelController : MonoBehaviour
         quaternion.eulerAngles = new Vector3(0, 0, Angle(targetDir) * -1);
 
         pointerCenter.transform.rotation = quaternion;
+
+        foreach (Transform t in wheelPieces)
+        {
+            t.GetComponent<Image>().color = Color.white;
+        }
+
+        //wheelPieces[GetSelectedID(Angle(targetDir))].GetComponent<Image>().color = Color.red;
+        wheelPieces[GetSelected(Angle(targetDir), wheelPieces.Length)].GetComponent<Image>().color = Color.red;
     }
 
-    public static float Angle(Vector2 vector2)
+    public void EmoteWheelKeybind(InputAction.CallbackContext callback)
+    {
+        if (callback.performed)
+        {
+
+        }
+
+        if (callback.canceled)
+        {
+
+        }
+    }
+
+
+    // Hard Coded
+    private int GetSelectedID(float angle)
+    {
+        float offSet = (360 / wheelPieces.Length) / 2;
+
+        if((angle > 0f && angle <= 45f - offSet) || (angle > 360 - offSet && angle <= 360))
+        {
+            return 0;
+        }
+        else if (angle > 45f - offSet && angle <= 90f - offSet)
+        {
+            return 1;
+        }
+        else if (angle > 90f - offSet && angle <= 135f - offSet)
+        {
+            return 2;
+        }
+        else if (angle > 135f - offSet && angle <= 180f - offSet)
+        {
+            return 3;
+        }
+        else if (angle > 180f - offSet && angle <= 225f - offSet)
+        {
+            return 4;
+        }
+        else if (angle > 225f - offSet && angle <= 270f - offSet)
+        {
+            return 5;
+        }
+        else if (angle > 270f - offSet && angle <= 315f - offSet)
+        {
+            return 6;
+        }
+        else if (angle > 315f - offSet && angle <= 360f - offSet)
+        {
+            return 7;
+        }
+
+        return -1;
+    }
+
+    // Modular (*￣3￣)╭
+    private int GetSelected(float angle, int pieces)
+    {
+        float pieceTreshold = 360 / pieces;
+        float offSet = pieceTreshold / 2;
+
+        if(pieces > 2)
+        {
+            // Special Case 0
+            if ((angle > 0f && angle <= pieceTreshold - offSet) || (angle > 360 - offSet && angle <= 360))
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < pieces; i++)
+            {
+                if (angle > (pieceTreshold * i) - offSet && angle <= pieceTreshold * (i + 1) - offSet)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+
+    }
+
+    private static float Angle(Vector2 vector2)
     {
         if (vector2.x < 0)
         {
