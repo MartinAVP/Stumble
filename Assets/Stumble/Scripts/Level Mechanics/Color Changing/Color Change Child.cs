83b9295c4f814974d481cee28602318bd250c41c
Shane M <Dragondragon.wu21@gmail.com>
2024-11-07 15:51:06 -0800

    Color Minigame Inital setp

---
+++ Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Change Child.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeChild : MonoBehaviour
+{
+    public bool triggered;
+    public bool chosen;
+    public bool endOfRound;
+    public GameObject self;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (chosen && triggered)
+        {
+
+        }
+        else if (triggered)
+        {
+            self.SetActive(false);
+        }
+        if(endOfRound && triggered && !chosen)
+        {
+            self.transform;
+        }
+        
+    }
+}
---
+++ Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Changing Minigame.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangingMinigame : MonoBehaviour
+{
+    public List<Color> colors = new List<Color>();
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // game starts
+
+    //populate block with certain info, color, letters etc
+    // > or have pre-exisiting blocks, prebuilt floor
+
+    // triggered
+
+    // color or trait is chosen
+
+    // display chosen color or trait
+
+    // timer counts down
+
+    // disable all blocks not with trait
+
+    // short timer
+
+    // reset floor - re enable blocks
+
+    // repeat
+
+
+
+}
