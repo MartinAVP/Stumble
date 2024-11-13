090b28b85fa8a1cc9ad14a92d13f91137ee67711
Martin Vilches Perez <129893589+MartinAVP@users.noreply.github.com>
2024-11-12 19:27:06 -0800

    GameModes

---
+++ Assets/Stumble/Scripts/NewDev/MapCard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MapCard : MonoBehaviour
+{
+    public Gamemode gamemode;
+    [Space]
+    public TextMeshProUGUI title;
+    public Image backgroundImage;
+}
