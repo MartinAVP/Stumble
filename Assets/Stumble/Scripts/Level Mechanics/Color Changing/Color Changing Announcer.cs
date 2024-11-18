fee3796cdf0bf470400bbd2d7422562a9586de09
Shane M <Dragondragon.wu21@gmail.com>
2024-11-17 17:34:47 -0800

    color cube temp

---
+++ Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Changing Announcer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangingAnnouncer : MonoBehaviour
+{
+    [SerializeField] ColorChangingMinigame colorMinigame;
+
+    private Color DisplayColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
--- Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Changing Minigame.cs
+++ Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Changing Minigame.cs
@@ -4,20 +4,63 @@ using UnityEngine;
 
 public class ColorChangingMinigame : MonoBehaviour
 {
-    public List<Color> colors = new List<Color>();
-    
+    [SerializeField] public List<Color> colors = new List<Color>();
+
+    [HideInInspector] public Color SelectedColor;
+
+    public int randomColorInt;
+
+    public int RoundsUntilNewColor = 2;
+    //
+    public int ColorsInPlay = 3;
+
+    public int RoundCounter = 0;
+
+
+    public float progressionSpeed;
+    private int progressionInt = 4;
+
+    public float TimeUntilAnnouncerDisapears = 2;
+    public float TimeUntilFloorDisapears = 3;
+
+    private bool timeToTrigger = false;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("A");
+
+        progressionInt = ColorsInPlay + 1;
+
+        for (int i = colors.Count; i < colors.Count; i++)
+        {
+            Debug.Log("Color:  " +colors[i]);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private IEnumerator ColorChanger()
+    {
+        RoundCounter++;
+        if (RoundCounter == RoundsUntilNewColor)
+        {
+            progressionInt += 1;
+            ColorsInPlay += 1;
+            RoundCounter = 0;
+        }
+        
+        randomColorInt = Random.Range(0, progressionInt);
+        SelectedColor = colors[randomColorInt];
+
+        yield return new WaitForSecondsRealtime(frameDelay);
     }
 
 
@@ -43,5 +86,8 @@ public class ColorChangingMinigame : MonoBehaviour
     // repeat
 
 
+    //in case of color blindness, need icons to help differentate colors
+    // > textures?
+    // > limiting colors? if green no red? if blue no yllow?
 
 }
