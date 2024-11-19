7030df9c97302b32a250fdca23daac1e26ceec98
Shane M <Dragondragon.wu21@gmail.com>
2024-11-18 23:47:04 -0800

    Hopefully its finished

--- Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Changing Minigame.cs
+++ Assets/Stumble/Scripts/Level Mechanics/Color Changing/Color Changing Minigame.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class ColorChangingMinigame : MonoBehaviour
 {
     [SerializeField] public List<Color> colors = new List<Color>();
-    [SerializeField] private GameObject cubePrefab; 
-    [SerializeField] private int gridWidth = 5;     
-    [SerializeField] private int gridHeight = 5;    
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private int gridWidth = 5;
+    [SerializeField] private int gridHeight = 5;
     [SerializeField] private float spacing = 1.5f;  // Spacing 
     [HideInInspector] public Color SelectedColor;
     private Color PostColor;
 
     public int randomColorInt;
-    
 
     public int RoundsUntilNewColor = 2;
-    private bool roundFinished = false;
+    public bool noImmediateRepeats = false;
     public int ColorsInPlay = 3;
 
     public int RoundCounter = 0;
@@ -38,15 +37,14 @@ public class ColorChangingMinigame : MonoBehaviour
     {
         Debug.Log("A");
 
-        progressionInt = ColorsInPlay + 1;
+        progressionInt = Mathf.Min(ColorsInPlay, colors.Count);
 
         for (int i = 0; i < colors.Count; i++)
         {
-            Debug.Log("Color: " +colors[i]);
+            Debug.Log("Color: " + colors[i]);
         }
 
         GenerateGrid();
-
     }
 
     // Update is called once per frame
@@ -60,6 +58,10 @@ public class ColorChangingMinigame : MonoBehaviour
 
     private void GenerateGrid()
     {
+        List<Color> colorPool = GenerateColorPool(gridWidth * gridHeight, progressionInt);
+        ShuffleList(colorPool);
+
+        int index = 0;
         for (int x = 0; x < gridWidth; x++)
         {
             for (int z = 0; z < gridHeight; z++)
@@ -67,12 +69,12 @@ public class ColorChangingMinigame : MonoBehaviour
                 Vector3 position = new Vector3(x * spacing, 0, z * spacing);
                 GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity, transform);
                 instantiatedCubes.Add(cube);
-                int randomIndex = Random.Range(0, ColorsInPlay);
-                Color randomColor = colors[randomIndex];
+
                 Renderer renderer = cube.GetComponent<Renderer>();
                 if (renderer != null)
                 {
-                    renderer.material.color = randomColor;
+                    renderer.material.color = colorPool[index];
+                    index++;
                 }
             }
         }
@@ -82,16 +84,29 @@ public class ColorChangingMinigame : MonoBehaviour
     {
         available = false;
         RoundCounter++;
-        if (RoundCounter == RoundsUntilNewColor && progressionInt < colors.Count)
+        if (RoundCounter == RoundsUntilNewColor)
         {
-            progressionInt += 1;
-            ColorsInPlay += 1;
+            progressionInt = Mathf.Min(progressionInt + 1, colors.Count);
+            ColorsInPlay = Mathf.Min(ColorsInPlay + 1, colors.Count);
             RoundCounter = 0;
         }
-        
-        
-        randomColorInt = Random.Range(0, progressionInt);
-        SelectedColor = colors[randomColorInt];
+
+        List<Color> colorPool = GenerateColorPool(instantiatedCubes.Count, ColorsInPlay);
+        ShuffleList(colorPool);
+
+        PostColor = SelectedColor;
+
+        if (noImmediateRepeats)
+        {
+            while (SelectedColor == PostColor)
+            {
+                SelectedColor = colorPool[Random.Range(0, colorPool.Count)];
+            }
+        }
+        else
+        {
+            SelectedColor = colorPool[Random.Range(0, colorPool.Count)];
+        }
 
         yield return new WaitForSecondsRealtime(TimerFirstHalf);
 
@@ -121,21 +136,62 @@ public class ColorChangingMinigame : MonoBehaviour
 
     private void ResetCubes()
     {
+        List<Color> colorPool = GenerateColorPool(instantiatedCubes.Count, ColorsInPlay);
+        ShuffleList(colorPool);
+
+        int index = 0;
         foreach (GameObject cube in instantiatedCubes)
         {
             if (cube != null)
             {
-                // Reactivate the cube
                 cube.SetActive(true);
-
-                // Assign a new random color
                 Renderer renderer = cube.GetComponent<Renderer>();
                 if (renderer != null)
                 {
-                    int randomIndex = Random.Range(0, ColorsInPlay);
-                    renderer.material.color = colors[randomIndex];
+                    renderer.material.color = colorPool[index];
+                    index++;
                 }
             }
+        }
+    }
+
+    private List<Color> GenerateColorPool(int totalCubes, int colorsInPlay)
+    {
+        List<Color> colorPool = new List<Color>();
+        int actualColorsInPlay = Mathf.Min(colorsInPlay, colors.Count);
+
+        int cubesPerColor = totalCubes / actualColorsInPlay;
+        int remainder = totalCubes % actualColorsInPlay;
+
+        for (int i = 0; i < actualColorsInPlay; i++)
+        {
+            for (int j = 0; j < cubesPerColor; j++)
+            {
+                colorPool.Add(colors[i]);
+            }
+        }
+
+        for (int i = 0; i < remainder; i++)
+        {
+            colorPool.Add(colors[Random.Range(0, actualColorsInPlay)]);
+        }
+
+        while (colorPool.Count < totalCubes)
+        {
+            colorPool.Add(colors[Random.Range(0, actualColorsInPlay)]);
+        }
+
+        return colorPool;
+    }
+
+    private void ShuffleList<T>(List<T> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            T temp = list[i];
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
         }
     }
 
