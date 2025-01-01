fc29fc96584277ebd6257fa70a1cab30e284d711
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2024-12-31 19:51:27 -0800

    Ready to re record trailer

--- Assets/Stumble/Scripts/TrailerRecording/TriggerCameraPan.cs
+++ Assets/Stumble/Scripts/TrailerRecording/TriggerCameraPan.cs
@@ -1,13 +1,27 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class TriggerCameraPan : MonoBehaviour
 {
-    public GameObject cameraPanObject;
+    public GameObject[] turnOnObjects;
+    public CinemachineDollyCart[] dollyCarts;
+    public float dollySpeed = 0.075f;
 
     private void OnTriggerEnter(Collider other)
     {
-        cameraPanObject.SetActive(true);
+        if (!other.CompareTag("Player")) return;
+
+        foreach (var turnOnObject in turnOnObjects)
+        {
+            turnOnObject.SetActive(true);
+        }
+
+        foreach (var dollyObject in dollyCarts)
+        {
+            dollyObject.m_Speed = dollySpeed;
+        }
     }
 }
